147da79caf06e253387c73eb90e88d1cb7b3d27b
Tu Nombre <sammygapa@gmail.com>
2026-08-11 19:39:17 -0500

    icono y message box ts

--- frontend/sepsense-login/src/app/shared/components/message-box/message-box.ts
+++ frontend/sepsense-login/src/app/shared/components/message-box/message-box.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,input,output } from '@angular/core';
 
 @Component({
   selector: 'app-message-box',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './message-box.scss',
 })
 export class MessageBox {
+tittle=input('confirmar accion');
+message=input('¿Estas seguro de realizar esta accion?');
+confirmtext=input ("Confirmar")
+canceltext=input("Cancelar");
 
+confirm=output<void>();
+cancelar=output<void>();
 }
