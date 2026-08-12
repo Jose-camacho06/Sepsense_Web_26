3b0ff26b43c7e8aa57e7ccf2e245d4760fddbe7e
Tu Nombre <sammygapa@gmail.com>
2026-08-11 19:24:21 -0500

    carpetas, login html , login typescript, no puedo correr la web

--- frontend/sepsense-login/src/app/app.routes.ts
+++ frontend/sepsense-login/src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/pages/login/login').then(
+        (component) => component.Login,
+      ),
+  },
+  {
+    path: 'registro',
+    loadComponent: () =>
+      import('./features/auth/pages/register/register').then(
+        (component) => component.Register,
+      ),
+  },
+  {
+    path: 'recuperar-contrasena',
+    loadComponent: () =>
+      import('./features/auth/pages/forgot-password/forgot-password').then(
+        (component) => component.ForgotPassword,
+      ),
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/pages/home/home').then(
+        (component) => component.Home,
+      ),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
