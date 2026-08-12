5e4103d4042d4a386d8366dd92f25deccbb59487
Jose <camacho.jose@uces.edu.co>
2026-08-11 21:01:48 -0500

    Arreglar validaciones y estilos del login

--- frontend/sepsense-login/src/app/features/auth/pages/login/login.ts
+++ frontend/sepsense-login/src/app/features/auth/pages/login/login.ts
@@ -1,7 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
- 
+import {Router} from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';  
+import { required } from '@angular/forms/signals';
+
+// ##Para formularios de inicio
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule],
@@ -11,28 +13,23 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 export class Login {
   private fb = inject(FormBuilder);
   private router = inject(Router);
- 
-  userTest = 'Jesus';
-  password = '1234567';
- 
-  form = this.fb.group({
-    user: ['', Validators.required],
-    //email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
+  userTest = "Jose"
+  password="1234567"
+
+  form =this.fb.group({
+    user: ['',Validators.required],
+    password:['',Validators.required],
+  
   });
- 
   initLogin() {
-    console.log('Login initialized');
-    //window.location.href = '/home';
-    this.router.navigate(['/home']);
-  }
- 
-  submit(){
-    if (this.form.valid) {
-      console.log('Form submitted', this.form.value);
-      // Handle form submission logic here
-      this.router.navigate(['/home']);
-    }
-    }
+  console.log('Login Iniciado');
+  //window.location.href = '/home';
+  this.router.navigate(['/home']);
+
 }
+submit(){
+  
+}
+}
+
  
