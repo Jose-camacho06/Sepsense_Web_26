0d73e762c71bf50133597e9fc430c35e9783538c
Tu Nombre <sammygapa@gmail.com>
2026-08-12 21:16:02 -0500

    login medio listoo e icono

--- frontend/sepsense-login/src/app/features/auth/pages/login/login.ts
+++ frontend/sepsense-login/src/app/features/auth/pages/login/login.ts
@@ -1,35 +1,42 @@
-import { Component, inject } from '@angular/core';
-import {Router} from '@angular/router';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';  
-import { required } from '@angular/forms/signals';
 
-// ##Para formularios de inicio
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss'],
 })
 export class Login {
   private fb = inject(FormBuilder);
   private router = inject(Router);
-  userTest = "Jose"
-  password="1234567"
 
-  form =this.fb.group({
-    user: ['',Validators.required],
-    password:['',Validators.required],
-  
+  form = this.fb.group({
+    user: ['', Validators.required],
+    password: ['', Validators.required],
   });
-  initLogin() {
-  console.log('Login Iniciado');
-  //window.location.href = '/home';
-  this.router.navigate(['/home']);
 
-}
-submit(){
-  
-}
-}
+  // Cuando el usuario pulse "Ingresar"
+  ingresar() {
+    if (this.form.valid) {
+      console.log('Login Iniciado')
+      this.router.navigate(['/home']);
+    } else {
+      //this.errorMessage = 'Usuario o contraseña inválido';;
+    }
+  }
 
- 
+  // Navegar a la ruta de registro (ajusta al path real)
+  irRegistro() {
+    this.router.navigate(['/registro']);
+  }
+
+  // Navegar a recuperar contraseña (ajusta al path real)
+  irOlvideContrasena() {
+    this.router.navigate(['/recuperar-contrasena']);
+  }
+}
