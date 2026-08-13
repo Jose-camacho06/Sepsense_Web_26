c5b1e8cd34013a99caa2ac1c61ce34d14722dde6
Tu Nombre <sammygapa@gmail.com>
2026-08-12 21:50:51 -0500

    recuperar contrasena esta lindo y todo

--- frontend/sepsense-login/src/app/features/auth/pages/forgot-password/forgot-password.ts
+++ frontend/sepsense-login/src/app/features/auth/pages/forgot-password/forgot-password.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
-  selector: 'app-forgot-password',
-  imports: [],
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './forgot-password.html',
-  styleUrl: './forgot-password.scss',
+  styleUrls: ['./forgot-password.scss'],
 })
 export class ForgotPassword {
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+
+  form = this.fb.group({
+    email: ['', Validators.email],
+    codigo: ['', Validators.required],
+  });
+
+  // Cuando el usuario pulse "Ingresar"
+  ingresar() {
+    if (this.form.valid) {
+      console.log(' contraseña recuperada')
+      this.router.navigate(['/login']);
+    } else {
+      //this.errorMessage = 'correo o código inválido';;
+    }
+  }
+  irRegistro() {
+    this.router.navigate(['/registro']);
+  }
 
 }
