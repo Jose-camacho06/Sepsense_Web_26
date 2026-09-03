29e8fdf5b5aa2c58d5e89a704f7c883273225f5e
Jose <camacho.jose@uces.edu.co>
2026-09-03 11:24:12 -0500

    Clase3-09

--- frontend/sepsense-login/src/app/shared/header/header.ts
+++ frontend/sepsense-login/src/app/shared/header/header.ts
@@ -1,42 +1,59 @@
-import { Component, inject , signal} from '@angular/core';
-import { Router,NavigationEnd, RouterLink } from '@angular/router';
-import{filter} from 'rxjs';
-
-
+import { Component, inject, signal } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
+ 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+ 
+  imports: [],
+ 
   templateUrl: './header.html',
+ 
   styleUrl: './header.scss',
 })
 export class Header {
   private router = inject(Router);
+ 
   pageTitle = signal('Dashboard');
-
-
-  constructor(){
+ 
+  showProfileMenu = false;
+  showProfileModal = false;
+ 
+  constructor() {
     this.updatePageTitle();
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe(() => {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
       this.updatePageTitle();
     });
   }
-
-private updatePageTitle() {
-  const url = this.router.url;
-  if (url.includes('usuarios')) {
-    this.pageTitle.set('Usuarios');
-    return;
+ 
+  private updatePageTitle() {
+    const url = this.router.url;
+    if (url.includes('usuarios')) {
+      this.pageTitle.set('Usuarios');
+      return;
+    }
+    if (url.includes('dashboard')) {
+      this.pageTitle.set('Dashboard');
+      return;
+    }
   }
-if (url.includes('dashboard')) {
-    this.pageTitle.set('Dashboard');
-    return;
+ 
+  toggleProfileMenu() {
+    this.showProfileMenu = !this.showProfileMenu;
+    console.log('showProfileMenu:', this.showProfileMenu);
   }
-  if (url.includes('home')) {
-    this.pageTitle.set('Home');
-    return;
+ 
+  openProfileModal() {
+    this.showProfileModal = true;
+    this.showProfileMenu = false;
+  }
+ 
+  closeProfileModal() {
+    this.showProfileModal = false;
+  }
+ 
+  logout() {
+    sessionStorage.removeItem('isLoggedIn');
+    this.router.navigate(['/login']);
   }
 }
-}
-  
