d2f136f9c8ac9ba8abe5d82c28a64ff51a5cd301
Tu Nombre <sammygapa@gmail.com>
2026-09-24 11:50:28 -0500

    conflicto resuelto

--- frontend/sepsense-login/src/app/shared/components/message-box/message-box.ts
+++ frontend/sepsense-login/src/app/shared/components/message-box/message-box.ts
@@ -8,17 +8,10 @@ import { Component,input,output } from '@angular/core';
 })
 export class MessageBox {
 tittle=input('confirmar accion');
-message=input ("¿Estas seguro de realizar esta accion?"); 
-confirmtext=input ("Confirmar"); 
-canceltext=input('Cancelar');
-//errorMessage=input('USUARIO O CONTRASEÑA INVALIDO');
-tittle=input('confirmar accion');
 message=input('¿Estas seguro de realizar esta accion?');
 confirmtext=input ("Confirmar")
 canceltext=input("Cancelar");
 
-confirm=output<void>(); 
-cancel=output<void>();
 confirm=output<void>();
 cancelar=output<void>();
 }
